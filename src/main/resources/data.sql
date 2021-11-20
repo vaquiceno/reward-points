58241fefee942c39b008f6850582204ee6cc9c2f
VAQUICENO030238\vaquiceno <vaquiceno@nexos-software.com.co>
2021-11-20 16:13:37 -0500

    modification of initial data set

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,28 @@
 insert into user(name) values ('pep1');
 insert into user(name) values ('pep2');
 
-insert into transaction(value, created, user_id) values (80, NOW() - 300 , 1);
-insert into transaction(value, created, user_id) values (40, NOW() - 100, 2);
-insert into transaction(value, created, user_id) values (0, NOW() - 50, 2);
+insert into transaction(value, created, user_id) values (150, NOW() - 10 , 1);
+insert into transaction(value, created, user_id) values (100, NOW() - 20 , 1);
+insert into transaction(value, created, user_id) values (80, NOW() - 30 , 1);
+insert into transaction(value, created, user_id) values (50, NOW() - 40 , 1);
+insert into transaction(value, created, user_id) values (40, NOW() - 50 , 1);
+insert into transaction(value, created, user_id) values (200, NOW() - 60 , 1);
+insert into transaction(value, created, user_id) values (300, NOW() - 70 , 1);
+insert into transaction(value, created, user_id) values (130, NOW() - 80 , 1);
+insert into transaction(value, created, user_id) values (110, NOW() - 90 , 1);
+insert into transaction(value, created, user_id) values (70, NOW() - 100 , 1);
+insert into transaction(value, created, user_id) values (10, NOW() - 110 , 1);
+insert into transaction(value, created, user_id) values (30, NOW() - 120 , 1);
+
+insert into transaction(value, created, user_id) values (200, NOW() - 10 , 2);
+insert into transaction(value, created, user_id) values (300, NOW() - 20 , 2);
+insert into transaction(value, created, user_id) values (400, NOW() - 30 , 2);
+insert into transaction(value, created, user_id) values (150, NOW() - 40 , 2);
+insert into transaction(value, created, user_id) values (12, NOW() - 50 , 2);
+insert into transaction(value, created, user_id) values (43, NOW() - 60 , 2);
+insert into transaction(value, created, user_id) values (56, NOW() - 70 , 2);
+insert into transaction(value, created, user_id) values (136, NOW() - 80 , 2);
+insert into transaction(value, created, user_id) values (412, NOW() - 90 , 2);
+insert into transaction(value, created, user_id) values (20, NOW() - 100 , 2);
+insert into transaction(value, created, user_id) values (436, NOW() - 110 , 2);
+insert into transaction(value, created, user_id) values (154, NOW() - 120 , 2);
