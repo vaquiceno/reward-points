b9967b35c69daf228398b17454b7d599149a8620
VAQUICENO030238\vaquiceno <vaquiceno@nexos-software.com.co>
2021-11-20 01:28:03 -0500

    add some log information

--- src/main/java/com/overactive/reward/rewardpoints/service/TransactionService.java
+++ src/main/java/com/overactive/reward/rewardpoints/service/TransactionService.java
@@ -4,12 +4,14 @@ import com.overactive.reward.rewardpoints.exception.BusinessException;
 import com.overactive.reward.rewardpoints.exception.TransactionNotFoundException;
 import com.overactive.reward.rewardpoints.model.Transaction;
 import com.overactive.reward.rewardpoints.repository.TransactionRepository;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
 
+@Log4j2
 @Service
 @Transactional
 public class TransactionService {
@@ -20,6 +22,7 @@ public class TransactionService {
 
     public Transaction addTransaction(Transaction transaction) throws BusinessException {
         try {
+            log.info("Saving user..");
             return transactionRepository.save(transaction);
         } catch (Exception e){
             throw new BusinessException(e.getMessage());
@@ -31,8 +34,10 @@ public class TransactionService {
             Transaction transaction1 = transactionRepository.findTransactionById(transaction.getId())
                     .orElseThrow(() -> new TransactionNotFoundException("User with id " + transaction.getId() + " not found"));
             if (!transaction1.equals(transaction)) {
+                log.info("Updating transaction with id " + transaction.getId());
                 return transactionRepository.save(transaction);
             }
+            log.info("transaction was not updated");
             return transaction1;
         } catch (Exception e){
             throw new BusinessException(e.getMessage());
--- src/main/java/com/overactive/reward/rewardpoints/service/UserService.java
+++ src/main/java/com/overactive/reward/rewardpoints/service/UserService.java
@@ -5,20 +5,17 @@ import com.overactive.reward.rewardpoints.exception.UserNotFoundException;
 import com.overactive.reward.rewardpoints.model.User;
 import com.overactive.reward.rewardpoints.repository.UserRepository;
 import com.overactive.reward.rewardpoints.utils.Util;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.sql.Timestamp;
-import java.time.LocalDate;
-import java.time.Month;
 import java.time.YearMonth;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.stream.Collectors;
-
+@Log4j2
 @Service
 @Transactional
 public class UserService {
@@ -32,6 +29,7 @@ public class UserService {
 
     public User addUser(User user) throws BusinessException {
         try {
+            log.info("Saving user..");
             return userRepository.save(user);
         } catch (Exception e){
             throw new BusinessException(e.getMessage());
@@ -43,8 +41,10 @@ public class UserService {
             User user1 = userRepository.findUserById(user.getId())
                     .orElseThrow(() -> new UserNotFoundException("User with id " + user.getId() + " not found"));
             if (!user1.equals(user)) {
+                log.info("Updating user with id " + user.getId());
                 return userRepository.save(user);
             }
+            log.info("User was not updated");
             return user1;
         } catch (Exception e){
             throw new BusinessException(e.getMessage());
@@ -55,6 +55,7 @@ public class UserService {
         try {
             User user = userRepository.findUserById(user_id)
                     .orElseThrow(() -> new UserNotFoundException("User with id "+ user_id +" not found"));
+            log.info("Calculating reward points");
             return Util.acumRewardPoints(user, from, to);
         } catch (Exception e){
             throw new BusinessException(e.getMessage());
@@ -70,6 +71,7 @@ public class UserService {
             YearMonth ym = YearMonth.now();
             int currentMonth = ym.getMonth().getValue();
             int currentYear = ym.getYear();
+            log.info("looking for previous months");
             for (int i = 0; i < numberPeriods; i++){
                 ym = YearMonth.of(currentYear, currentMonth);
                 mapReward.put(
@@ -83,6 +85,7 @@ public class UserService {
                     currentYear -= 1;
                 }
             }
+            log.info("information was retrieved successfully");
             finalMap.put("year-month", mapReward);
             finalMap.put("total",
                         mapReward
